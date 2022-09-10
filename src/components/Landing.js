5204975c5746f00354f74f425d8940c5b9e91c23
ShannonTao <shannon.cxq@gmail.com>
2022-09-10 03:04:05 -0400

    completed landing page and social links

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,35 @@
+import { FaInstagram, FaLinkedinIn, FaGithub } from "react-icons/fa";
+
 function Landing() {
   return (
     <section className="landingSection">
       <div className="wrapper">
-        <h1>Hi, my name is Shannon</h1>
+        <h1>
+          Hello, I'm <span className="strongName">Shannon Tao</span> Front-End
+          Web Developer <span className="greyText">in Toronto</span>
+        </h1>
+        <div className="socialLinks">
+          <ul>
+            <li>
+              <a href="#">
+                <FaLinkedinIn />
+                <span className="sr-only">Link to LinkedIn</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <FaGithub />
+                <span className="sr-only">Link to GitHub</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <FaInstagram />
+                <span className="sr-only">Link to Instagram</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
   );
